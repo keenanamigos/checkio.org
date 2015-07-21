91cd5c8f4b1b8cf11fd9f3ff27ebea90fd1cf83f
keenanamigos <keenan.johnson@rideamigos.com>
2015-07-21 13:47:02 -0400

    Finished solving problem3
    Needed to allow for floats in the even number case without using a float to reference a position in the list.

--- problem3.py
+++ problem3.py
@@ -1,33 +1,31 @@
 #Median
-x = [1,2,3,4,5]
+
 def test():
-   numbers = [1,2,3,4]
+   numbers = [1,2,3,4,5]
    list = sorted(numbers)
    if len(list) < 1:
       return None
    if len(list) % 2 == 1:
-      return float(list[len(list)/2])
+      return list[len(list)//2]
    else:
-      highnum = float(list[len(list)/2-1])
-      lownum =  float(list[len(list)/2])
-      return (highnum + lownum)/2
+      highnum = list[len(list)//2]
+      lownum =  list[len(list)//2-1]
+      return (highnum + lownum)/2.0 #Allow for floats
 
-def checkio(): 
-   y = [1,2,3,4,5]
-   list = sorted(y)
+def checkio(data):
+   list = sorted(data) #Need to sort the list in order to eventually find the median
    if len(list) < 1:
       return None
    if len(list) % 2 == 1:
-      return list[(len(list)+1)/2-1]
+      return list[len(list)//2] #Will round up to the correct number because it returns an int (keeps the indicies as ints)
    else:
-      highnum = list[len(list)/2-1] 
-      lownum =  list[len(list)/2]
-      return (highnum + lownum)/2 
-#Checkio didn't allow list indices to be floats
-#Unfinished
+      highnum =  list[len(list)// 2] #Keeps list indices as ints, returns "higher" number to be added then divided
+      lownum = list[len(list)// 2-1] #Keeps list indices as ints, returns "lower" number to be added then divided
+      return (highnum + lownum)/ 2.0 #Will be able to return floats for the even values
 
 
 
       
       
+ 
  
